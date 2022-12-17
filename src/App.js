570c23148f1d35487f0b7f619291a4b391a90c91
Anurag Sahu <anishsahu33@gmail.com>
2022-12-17 16:06:32 +0530

    API done but UI meh problem hai

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { StreamChat } from "stream-chat";
+import { Channel, ChannelHeader, Chat, MessageInput, MessageList, Window } from "stream-chat-react";
+import 'stream-chat-react/dist/css/index.css';
+
+const API_KEY = process.env.REACT_APP_API_KEY
+
+const USER1 = {
+  id: "user1",
+  name: "user1",
+  image: "https://picsum.photos/id/64/200/300"
+}
+const USER2 = {
+  id: "user2",
+  name: "user2",
+  image: "https://picsum.photos/id/65/200/300"
+}
+const USER3 = {
+  id: "user3",
+  name: "user3",
+  image: "https://picsum.photos/id/73/200/300"
+}
+
+const users = [USER1, USER2, USER3];
+
+//Generating random index to select random user
+const getRandomUser = () => {
+  const randomIndex = Math.floor(Math.random() * users.length);
+  return users[randomIndex];
+};
+
 
 function App() {
+  const [chatClient, setChatClient] = useState(null);
+  const [channel,setChannel] = useState(null);
+
+  useEffect(() => {
+   async function initChat() {
+      const client = StreamChat.getInstance(API_KEY);
+
+      //connecting to user
+      const user = getRandomUser();
+
+      client.connectUser(user, client.devToken(user.id));
+
+      //creatiing Channels
+      const channel = client.channel('team','general',{
+        name: "General", //channel name
+        image: "https://picsum.photos/id/75/200/300",
+      })
+
+      await channel.create();
+      channel.addMembers([user.id]);
+      setChannel(channel);
+
+
+      setChatClient(client);
+
+    }
+
+    initChat();
+
+    //Cleanup function...once done..disconnect the user
+    return () =>{
+      if(chatClient) chatClient.disconnectUser();
+    }
+  }, [])
+
+  if (!chatClient || !channel) return <></>
+
   return (
-    <div className="App">
-      <h1>Test</h1>
-      {/* Above is a test */}
-      <h1>Test3</h1>
+    <div>
+      <Chat client={chatClient} theme={'messaging light'}>
+        <Channel channel = {channel}> 
+          <Window>
+            <ChannelHeader/>
+            <MessageList/>
+            <MessageInput/>
+          </Window>
+        </Channel>
+      </Chat>
     </div>
   );
 }
